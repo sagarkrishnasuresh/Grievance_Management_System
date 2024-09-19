dff3bf3959520d4b2e49a5d3146df665b13929f2
S Sagar Krishna <84390223+sagarkrishnasuresh@users.noreply.github.com>
2024-09-19 14:17:22 +0530

    Delete grievance-management directory

--- grievance-management/grievance-management/src/main/java/com/grievance/reposistory/GrievanceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.grievance.repository;
-
-import com.grievance.model.Grievance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GrievanceRepository extends JpaRepository<Grievance, Long> {
-}
--- grievance-management/grievance-management/src/main/java/com/grievance/reposistory/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.grievance.repository;
-
-import com.grievance.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserEmail(String email);
-}
